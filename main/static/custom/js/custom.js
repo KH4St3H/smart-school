2f735a9c14b3d6af4f2fccaeabcab66e0dcb5896
Mehrshad <mehrshadfiroozian@gmail.com>
2020-02-20 20:35:45 +0330

    update templates

--- main/static/custom/js/custom.js
+++ main/static/custom/js/custom.js
@@ -1,18 +1,29 @@
 $(document).ready(function () {
     $("button").click(function () {
         let id = this.id;
-        $("#"+this.id).prop('disabled', true);
-        $("#"+id.slice(0, -1)).removeClass("w3-dark-gray");
         if(id.slice(-1)==='p'){
+            $("#"+this.id).prop('disabled', true);
+            $("#"+id.slice(0, -1)).removeClass("w3-dark-gray");
             $("#"+id.slice(0, -1)).removeClass("w3-red");
             $("#"+id.slice(0, -1)).addClass('w3-green');
             // $("#"+id.slice(0, -1)).addClass('w3-light-green');
             $("#"+id.slice(0, -1)+"a").prop('disabled', false);
-        }else{
+        }else if(id.slice(-1)==='a'){
+            $("#"+this.id).prop('disabled', true);
+            $("#"+id.slice(0, -1)).removeClass("w3-dark-gray");
             $("#"+id.slice(0, -1)).removeClass("w3-green");
             $("#"+id.slice(0, -1)).addClass('w3-red');
             // $("#"+id.slice(0, -1)).addClass('w3-light-green');
             $("#"+id.slice(0, -1)+"p").prop('disabled', false);
+        }else{
+            return true;
         }
     });
 });
+
+function select_all(student_count) {
+    // alert(student_count);
+    for (i = 1; i<student_count+1; i++){
+        $("#"+i.toString()+"p").click();
+    }
+}
